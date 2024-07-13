c3f4cfac02b8485470d3a17774cda5edea291b15
nitesh030923 <niteshpatnaik03@gmail.com>
2024-07-13 06:26:23 +0530

    Made changes to the architecture

--- classification.py
+++ classification.py
@@ -1,8 +1,9 @@
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization
+from tensorflow.keras.layers import GlobalAveragePooling2D, Dense, Dropout
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.applications import MobileNetV2
 import os
 
 # Define path
@@ -32,32 +33,32 @@ validation_generator = datagen.flow_from_directory(
     class_mode='binary',
     subset='validation'
 )
+# Load the pre-trained MobileNetV2 model + higher level layers
+base_model = MobileNetV2(weights='imagenet', include_top=False, input_shape=(128, 128, 3))
 
+# Freeze the first 100 layers of the model
+for layer in base_model.layers[:100]:
+    layer.trainable = False
 # Simplified CNN architecture
 def create_model():
     model = Sequential([
-        Conv2D(16, (3, 3), activation='relu', input_shape=(128, 128, 3)),
-        MaxPooling2D(pool_size=(2, 2)),
-        
-        Conv2D(32, (3, 3), activation='relu'),
-        MaxPooling2D(pool_size=(2, 2)),
-        
-        Flatten(),
-        Dense(64, activation='relu'),
-        Dropout(0.5),
-        Dense(1, activation='sigmoid')
+         base_model,
+         GlobalAveragePooling2D(),
+         Dense(128, activation='relu'),
+         Dropout(0.5),
+         Dense(1, activation='sigmoid')
     ])
     
     return model
 
 # Instantiate and compile the model
 model = create_model()
-model.compile(optimizer=Adam(learning_rate=0.001), loss='binary_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=Adam(learning_rate=0.0001), loss='binary_crossentropy', metrics=['accuracy'])
 
 # Train the model
 model.fit(
     train_generator,
-    epochs=20,
+    epochs=30,
     validation_data=validation_generator
 )
 
